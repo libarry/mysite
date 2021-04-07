bb4ddb9465dbfe5381e405eb0713f30dc0b45a02
libarry <870390541@qq.com>
2021-04-07 20:47:03 +0800

    fix as_view() args in urls.py

--- novels/urls.py
+++ novels/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('book/create',
         views.BookCreateView.as_view(), name='book_create'),
     path('book/<int:pk>/update',
-         views.BookUpdateView.as_view(success_url=reverse_lazy('novels:all')), name='book_update'),
+         views.BookUpdateView.as_view(), name='book_update'),
     path('book/<int:pk>/delete',
-        views.BookDeleteView.as_view(success_url=reverse_lazy('novels:all')), name='book_delete'),
+        views.BookDeleteView.as_view(), name='book_delete'),
 
     path('chapter/<int:ck>', views.ChapterDetailView.as_view(), name='chapter_detail'),
     path('book/<int:pk>/write', views.ChapterCreateView.as_view(), name='chapter_create'),
