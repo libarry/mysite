c0b0d5e8f0de02b085e73980130ccc74bf66d693
libarry <870390541@qq.com>
2021-04-07 21:47:04 +0800

    fix some bugs

--- novels/admin.py
+++ novels/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tag,Book,Fav,Chapter,Comment
+admin.site.register(Tag)
+admin.site.register(Book)
+admin.site.register(Fav)
+admin.site.register(Chapter)
+admin.site.register(Comment)
